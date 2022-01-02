d3b33eced2a9be9b787c18b89fe9f6c0246820f1
js4322 <js4322830@gmail.com>
2022-01-02 17:58:40 +0200

    Initial commit

---
+++ ControlPanelTest.java
@@ -0,0 +1,23 @@
+
+class ControlPanel {
+    private Altimeter altimeter = new Altimeter();
+    private AirGauge airGauge = new AirGauge();
+    public void control(){
+        System.out.println(altimeter.toString());
+        System.out.println(airGauge.toString());
+    }
+}
+
+class Altimeter {
+    @Override
+    public String toString(){
+        return "Measure height";
+    }
+}
+
+class AirGauge {
+    @Override
+    public String toString(){
+        return "Measure air pressure";
+    }
+}
